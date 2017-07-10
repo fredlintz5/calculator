90ac3ef8e564e43558d2237a4436cec9b1f79cc6
Frederick Lintz <fred.lintz5@gmail.com>
2017-07-09 22:19:11 -0600

    Update calc.js
    added comments to code

--- calc.js
+++ calc.js
@@ -1,13 +1,19 @@
+//wait for page to load before applying functions
 window.onload = function() {
 
+	//global variables to distinguish the display from the buttons
 	var display = document.getElementById('display');
 	var buttons = document.getElementById('buttons');
+	
+	//these are variables I was thinking i would use to control what characters can be pressed in the calculator	
 	//var regEx = /0-9./gi;
 	//var operators = /+-=*\//gi;
 
-
+	
+	//event listener for button clicks
 	buttons.addEventListener('click', function(event) {
 		
+		//creating a variable for the innerHTML of the button clicked
 		var value = event.target.innerHTML;
 
 		if (value === '=') {
@@ -27,9 +33,10 @@ window.onload = function() {
 			
 	});
 
-
+	//function to add an event listener to the document for any key presses, and displaying them
 	document.addEventListener('keypress', function(event) {
 	
+	//this variable converts the keyboard key's keycode to a string	
 	var inputKey = String.fromCharCode(event.keyCode);
 
 		if (event.keyCode === 13 || event.keyCode === 61) {
@@ -41,7 +48,8 @@ window.onload = function() {
 			display.innerHTML += inputKey;
 		}	
 	});
-
+	
+	//I had to create another function for the delete key, because the 'keypress" function wouldnt grab iy
 	document.addEventListener('keydown', function(event) {
 
 		if (event.keyCode === 8) {
